a3c54d235ee9776260d49e106245aafce06cf252
ik_2dm3 <ik_2dm3@h015110.net>
2017-11-24 12:47:49 +0100

    Commit prueba2

--- app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Mapa.java
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Mapa.java
@@ -3,22 +3,33 @@ package com.example.ik_2dm3.ijcapp;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.kml.KmlLayer;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class Activity_Mapa extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
     private static final String TAG = Activity_Mapa.class.getSimpleName();
     private static final String TAG1 = "ERROR";
 
@@ -44,37 +55,72 @@ public class Activity_Mapa extends FragmentActivity implements OnMapReadyCallbac
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        // Add a marker in Arrigorriaga and move the camera
+        mMap=googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng AyuntamientoArrigorriaga = new LatLng(43.205889, -2.887724);
+
+        mMap.addMarker(new MarkerOptions().position(AyuntamientoArrigorriaga).title("Ayuntamiento de Arrigorriaga").icon(BitmapDescriptorFactory.fromResource(R.drawable.pregunta)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(AyuntamientoArrigorriaga));
+
+   /*
+   try {
+       // Customise the styling of the base map using a JSON object defined
+       // in a raw resource file.
+       boolean success = googleMap.setMapStyle(
+               MapStyleOptions.loadRawResourceStyle(
+                       this, R.raw.style_json));
+
+       if (!success) {
+           Log.e(TAG, "Style parsing failed.");
+       }
+
+   } catch (Resources.NotFoundException e) {
+       Log.e(TAG, "Can't find style. Error: ", e);
+   }
+   */
 
         try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.style_json));
-
-            if (!success) {
-                Log.e(TAG, "Style parsing failed.");
-            }
-
-        } catch (Resources.NotFoundException e) {
-            Log.e(TAG, "Can't find style. Error: ", e);
+            KmlLayer layer = new KmlLayer(mMap, R.raw.arrigorriagamunicipio, getApplicationContext());
+            layer.addLayerToMap();
+        } catch (XmlPullParserException | IOException e) {
+            Log.e(TAG, " Error: ", e);
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
             mMap.setMyLocationEnabled(true);
-        } else {
-            // Show rationale and request permission.
+        }else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
         }
 
-        //KmlLayer layer = new KmlLayer(getMap(), R.raw.kmlFile, getApplicationContext());
-        //layer.addLayerToMap();
 
+        }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Esta app requiere que se concedan los permisos de localización", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                }
+                break;
+        }
     }
+
 }
