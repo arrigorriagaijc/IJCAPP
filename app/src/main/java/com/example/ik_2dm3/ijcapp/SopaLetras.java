dbd8b31de6cf21e82e0e32c59099c411ef509a6d
ik_2dm3 <ik_2dm3@h015110.net>
2017-11-27 12:47:14 +0100

    Creadas las funciones: -ObtenerLocalizacion() -EstablecerPermisos() -ActivarGps() Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias.
    Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click.
    Empezamos con la sopa de letras.

--- app/src/main/java/com/example/ik_2dm3/ijcapp/SopaLetras.java
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/SopaLetras.java
@@ -3,11 +3,11 @@ package com.example.ik_2dm3.ijcapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OuterClass extends AppCompatActivity {
+public class SopaLetras extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outer_class);
+        setContentView(R.layout.sopaletras);
     }
 }
