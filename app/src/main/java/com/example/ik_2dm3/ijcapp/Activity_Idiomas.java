55f145b581a83c637528296d0131e2d0a0089515
ik_2dm3 <ik_2dm3@h015110.net>
2017-11-30 17:01:57 +0100

    -ObtenerLocalizacion() -EstablecerPermisos() -ActivarGps() Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias. Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click. Sopa de letras acabada. Detecta el touch y cambia de color las letras.Felicita al acertar. (Estaria bien solucionar el color amarillo Queda gestionar la vuelta al activity de mapa) -Añadido el activity de idiomas.

--- app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Idiomas.java
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Idiomas.java
@@ -2,6 +2,7 @@ package com.example.ik_2dm3.ijcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Activity_Idiomas extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class Activity_Idiomas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__idiomas);
+
+        Button btnEuskera=(Button) findViewById(R.id.btnEuskera);
+        Button btnCastellano=(Button) findViewById(R.id.btnEspañol);
+
+        btnEuskera.setWidth(btnCastellano.getWidth());
     }
 }
