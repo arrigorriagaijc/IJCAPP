0cdb63c5edf5ef20c3261feb819a473c1f4f648b
ik_2dm3 <ik_2dm3@h015110.net>
2017-11-30 12:29:59 +0100

    Creadas las funciones: -ObtenerLocalizacion() -EstablecerPermisos() -ActivarGps() Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias. Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click. Sopa de letras acabada. Detecta el touch y cambia de color las letras.Ya hce lineas rectas. Queda que solo se seleccione una linea y que no registre el mismo textView mas de una vez. -Creado el Activity_idiomas.

---
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Idiomas.java
@@ -0,0 +1,13 @@
+package com.example.ik_2dm3.ijcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Idiomas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__idiomas);
+    }
+}
