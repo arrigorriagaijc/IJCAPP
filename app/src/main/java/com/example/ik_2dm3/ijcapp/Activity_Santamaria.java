461a1866bd39a73ecd1711b81cd8634866338e2d
ik_2dm3 <ik_2dm3@h015110.net>
2017-12-12 22:23:04 +0100

    -ObtenerLocalizacion()                               -EstablecerPermisos()                               -ActivarGps()                               Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias.                               Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click.                               Sopa de letras acabada. Detecta el touch y cambia de color las letras.Felicita al acertar. Muestra un dialog con una explicación al acertar cada letra, cuantas veces la marque.                               -Añadido el boton de flecha cuando aciertan todas.                               -Bloqueado el activity sopa de letras para que no gire.(Añadida linea a android studio que lo bloquea porque al añadir el boton, si se gira se ocultan un poco las letras y es una movida arreglarlo todo, olvidadlo)                               (Estaria bien solucionar el color amarillo                               Queda gestionar la vuelta al activity de mapa)                               -Añadido el activity de idiomas con su fondo.                               -La app arranca desde el activity de idiomas.                               -Mejorados los botones de idioma, de siguiente y de audio.                               -Añadido gif a Mikel Rico                               -Añadidas más cosas, toca revisar                               -Empezado activity Santa Maria: Creando

--- app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Santamaria.java
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Santamaria.java
@@ -1,13 +1,35 @@
 package com.example.ik_2dm3.ijcapp;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity_Santamaria extends AppCompatActivity {
+
+    private Button btnSiguiente;
+    private ConstraintLayout clEnunciado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__santamaria);
+
+        //Enunciado del ejercicio
+        btnSiguiente=(Button) findViewById(R.id.btnSiguiente);
+        clEnunciado=(ConstraintLayout) findViewById(R.id.clEnunciado);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clEnunciado.setVisibility(View.GONE);
+            }
+        });
+
+        //Primera pregunta
+
+
     }
 }
