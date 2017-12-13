49b2069badbbe833543267e425f92fe7596bfd78
ik_2dm3 <ik_2dm3@h015110.net>
2017-12-13 18:34:30 +0100

    -ObtenerLocalizacion()                               -EstablecerPermisos()                               -ActivarGps()                               Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias.                               Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click.                               Sopa de letras acabada. Detecta el touch y cambia de color las letras.Felicita al acertar. Muestra un dialog con una explicación al acertar cada letra, cuantas veces la marque.                               -Añadido el boton de flecha cuando aciertan todas.                               -Bloqueado el activity sopa de letras para que no gire.(Añadida linea a android studio que lo bloquea porque al añadir el boton, si se gira se ocultan un poco las letras y es una movida arreglarlo todo, olvidadlo)                               (Estaria bien solucionar el color amarillo                               Queda gestionar la vuelta al activity de mapa)                               -Añadido el activity de idiomas con su fondo.                               -La app arranca desde el activity de idiomas.                               -Mejorados los botones de idioma, de siguiente y de audio.                               -Añadido gif a Mikel Rico                               -Añadidas más cosas, toca revisar                               -Empezado activity Santa Maria: Creado enunciado y pregunta 1.

--- app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Santamaria.java
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Activity_Santamaria.java
@@ -1,5 +1,6 @@
 package com.example.ik_2dm3.ijcapp;
 
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +21,14 @@ public class Activity_Santamaria extends AppCompatActivity {
     private Button btnSiguiente;
     private ConstraintLayout clEnunciado;
     private ImageView ivRespuestaBuena;
+    private ImageView ivRespuestaMala;
     private ImageView ivAcierto;
+    private Button btnPregunta1;
+    private int duracion;
+    private Toast toast;
+    private LinearLayout llPregunta1;
+    private TextView tvRespuestaCorregida;
+    private LinearLayout llRespuestas1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +39,88 @@ public class Activity_Santamaria extends AppCompatActivity {
         btnSiguiente=(Button) findViewById(R.id.btnSiguiente);
         clEnunciado=(ConstraintLayout) findViewById(R.id.clEnunciado);
         ivRespuestaBuena=(ImageView) findViewById(R.id.ivRespuestaBuena);
+        ivRespuestaMala=(ImageView) findViewById(R.id.ivRespuestaMala);
+        btnPregunta1=(Button) findViewById(R.id.btnPregunta1);
+        llPregunta1=(LinearLayout) findViewById(R.id.llPregunta1);
+        tvRespuestaCorregida=(TextView) findViewById(R.id.tvRespuestaCorregida);
+        llRespuestas1=(LinearLayout) findViewById(R.id.llRespuestas1);
 
-        ivAcierto.setImageDrawable(getResources().getDrawable(R.drawable.tick));
-        ivAcierto.setMaxWidth(100);
-        ivAcierto.setMaxHeight(100);
-
+        //Pasamos a la primera pregunta
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Ocultamos el enunciado
                 clEnunciado.setVisibility(View.GONE);
+                //Mostramos la pregunta
+                llPregunta1.setVisibility(View.VISIBLE);
             }
         });
 
 
         //Primera pregunta
+        //Si acierta:
         ivRespuestaBuena.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast=new Toast(getApplicationContext());
-                toast.setGravity(Gravity.CLIP_HORIZONTAL, 0, 0);
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(ivAcierto);
+
+                //Cargamos el layout del tick
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.acierto,
+                        (ViewGroup) findViewById(R.id.clAcierto));
+
+                //Creamos el toast
+                toast = new Toast(getApplicationContext());
+                //Lo centramos
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                //Le ponemos duración corta
+                toast.setDuration(Toast.LENGTH_SHORT);
+                //Le asignamos el layout
+                toast.setView(layout);
+                //Lo mostramos
                 toast.show();
+
+                //Ocultamos la pregunta actual
+                llPregunta1.setVisibility(View.GONE);
             }
         });
+        //Si falla:
+        ivRespuestaMala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Cargamos el layout del tick
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.error,
+                        (ViewGroup) findViewById(R.id.clError));
+
+                //Creamos el toast
+                toast = new Toast(getApplicationContext());
+                //Lo centramos
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                //Le ponemos duración corta
+                toast.setDuration(Toast.LENGTH_SHORT);
+                //Le asignamos el layout
+                toast.setView(layout);
+                //Lo mostramos
+                toast.show();
+
+                //Ocultamos las respuestas
+                llRespuestas1.setVisibility(View.GONE);
+                //Mostramos la explicación:
+                tvRespuestaCorregida.setVisibility(View.VISIBLE);
+                //Mostramos el botón pregunta1:
+                btnPregunta1.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //Si pulsa en siguiente se muestra la pregunta 2
+        btnPregunta1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Ocultamos la pregunta1
+                llPregunta1.setVisibility(View.GONE);
+            }
+        });
+
 
 
     }
