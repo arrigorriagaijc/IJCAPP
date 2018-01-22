a73b218431202f57ecf0c32de4182e4fcee3ed15
ik_2dm3 <ik_2dm3@h015110.net>
2018-01-22 13:31:19 +0100

    -Puzzle arreglado y strings p.4

---
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/activity_pantalla4.java
@@ -0,0 +1,13 @@
+package com.example.ik_2dm3.ijcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_pantalla4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla4);
+    }
+}
