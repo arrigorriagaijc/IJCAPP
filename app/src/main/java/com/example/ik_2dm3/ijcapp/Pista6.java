b6bdcfa22f5681c3a72598d7d5715c44fac7aeec
ik_2dm3 <ik_2dm3@h015110.net>
2018-01-24 13:54:33 +0100

    -ObtenerLocalizacion()                                                                                                                                -EstablecerPermisos()                                                                                                                                -ActivarGps()                                                                                                                                Modificado el listener del botón "MyLocation" para que acerque el zoom a nuestra preferencias.                                                                                                                                Añadidos todos los marcadores con el icono de pregunta. Añadidos los listener en los marcadores para que cambie su imagen al hacer click.                                                                                                                                Sopa de letras acabada. Detecta el touch y cambia de color las letras.Felicita al acertar. Muestra un dialog con una explicación al acertar cada letra, cuantas veces la marque.                                                                                                                                -Añadido el boton de flecha cuando aciertan todas.                                                                                                                                -Bloqueado el activity sopa de letras para que no gire.(Añadida linea a android studio que lo bloquea porque al añadir el boton, si se gira se ocultan un poco las letras y es una movida arreglarlo todo, olvidadlo)                                                                                                                                (Estaria bien solucionar el color amarillo                                                                                                                                Queda gestionar la vuelta al activity de mapa)                                                                                                                                -Añadido el activity de idiomas con su fondo.                                                                                                                                -La app arranca desde el activity de idiomas.                                                                                                                                -Mejorados los botones de idioma, de siguiente y de audio.                                                                                                                                -Añadido gif a Mikel Rico                                                                                                                                -Añadidas más cosas, toca revisar                                                                                                                                -Empezado activity Santa Maria: Creado enunciado y pregunta 1.
                                                                                                     Strings corregidos y activity SantaMAria reformado hasta pregunta 3 (incluida).
                                                                                                     -BOtones de idioma cambiados(Queda cuadrarlos para tablets).
                                                                                                     -Puzzle terminado. Mejorable recogiendo los datos de la sombra en el draglistener pero funcional.
    -Pantalla 4 acabada.
    -Pista 6 acabada.

---
+++ app/src/main/java/com/example/ik_2dm3/ijcapp/Pista6.java
@@ -0,0 +1,13 @@
+package com.example.ik_2dm3.ijcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pista6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pista6);
+    }
+}
